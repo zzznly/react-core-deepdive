9ce14c1f802be30fe60536a29c99f024ca2539ea
zzznly <zzznly@gmail.com>
2025-01-16 19:51:44 +0900

    feat: App.jsx, Header, Main 컴포넌트 생성

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header/Header";
+import Main from "./components/section/Section";
+
+export default function App() {
+  return (
+    <div id="app">
+      <Header />
+      <Main />
+    </div>
+  );
+}
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main>
+      <h2>Main Component</h2>
+    </main>
+  );
+}
