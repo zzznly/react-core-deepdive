34f7d551b5fd26efcbe03afc76c8d3e7e93f1ae7
zzznly <zzznly@gmail.com>
2025-02-01 01:48:00 +0900

    refactor: render 함수 수정

--- libs/core/render.js
+++ libs/core/render.js
@@ -1,12 +1,86 @@
-import { createVirtualDom } from "./dom/create";
+export default function render(vdom, container, oldDom = container.firstChild) {
+  if (typeof vdom === "string" || typeof vdom === "number") {
+    if (oldDom && oldDom.nodeType === 3) {
+      if (oldDom.nodeValue !== String(vdom)) {
+        oldDom.nodeValue = vdom;
+      }
+    } else {
+      const textNode = document.createTextNode(vdom);
+      container.replaceChild(textNode, oldDom);
+    }
+    return;
+  }
 
-export const render = (vNode, rootContainer) => {
-  let currentVirtualDom;
-  let rootElement;
+  if (typeof vdom.type === "function") {
+    const componentVdom = vdom.type(vdom.props);
+    render(componentVdom, container, oldDom);
+    return;
+  }
 
-  rootElement = rootContainer; // rootElement를 최초 렌더(mount)시 등록
-  currentVirtualDom = vNode; // 현재 virtualDOM Node를 최초 mount된 컴포넌트로 등록
-  console.log("render - vNode", vNode);
-  const element = createVirtualDom(vNode);
-  rootContainer.appendChild(element);
-};
+  let domElement = oldDom;
+
+  if (!domElement || domElement.nodeName.toLowerCase() !== vdom.type) {
+    domElement = document.createElement(vdom.type);
+    if (oldDom) {
+      container.replaceChild(domElement, oldDom);
+    } else {
+      container.appendChild(domElement);
+    }
+  }
+
+  const prevProps = oldDom?._vdom?.props || {};
+  const nextProps = vdom.props || {};
+
+  // 1. 이전 props와 현재 props가 동일하면 렌더링 건너뛰기
+  if (!hasPropsChanged(prevProps, nextProps)) {
+    return;
+  }
+
+  updateProps(domElement, nextProps, prevProps);
+
+  const oldChildren = prevProps.children || [];
+  const newChildren = nextProps.children || [];
+
+  updateChildren(domElement, newChildren, oldChildren);
+
+  domElement._vdom = vdom;
+}
+
+// 2. props 변경 여부 확인 함수
+function hasPropsChanged(prevProps, nextProps) {
+  const prevKeys = Object.keys(prevProps);
+  const nextKeys = Object.keys(nextProps);
+
+  if (prevKeys.length !== nextKeys.length) return true;
+
+  return nextKeys.some((key) => prevProps[key] !== nextProps[key]);
+}
+
+// 3. updateProps 수정: 기본값 추가하여 오류 방지
+function updateProps(dom, newProps = {}, oldProps = {}) {
+  Object.keys(newProps)
+    .filter((key) => key !== "children")
+    .forEach((name) => {
+      if (newProps[name] !== oldProps[name]) {
+        dom[name] = newProps[name];
+      }
+    });
+
+  Object.keys(oldProps)
+    .filter((key) => key !== "children")
+    .forEach((name) => {
+      if (!(name in newProps)) {
+        dom[name] = null;
+      }
+    });
+}
+
+// 4. updateChildren 수정: 기본값 추가하여 오류 방지
+function updateChildren(dom, newChildren = [], oldChildren = []) {
+  newChildren = Array.isArray(newChildren) ? newChildren : [newChildren];
+  oldChildren = Array.isArray(oldChildren) ? oldChildren : [oldChildren];
+
+  for (let i = 0; i < newChildren.length || i < oldChildren.length; i++) {
+    render(newChildren[i], dom, dom.childNodes[i]);
+  }
+}
