0ac3775bf801f91df4de27ab76a27a524007385a
zzznly <zzznly@gmail.com>
2025-02-07 22:12:48 +0900

    refactor: createElement 메서드 수정 - 평탄화, 맵핑 제거, 함수컴포넌트 처리 수정

--- libs/jsx/jsx-runtime.ts
+++ libs/jsx/jsx-runtime.ts
@@ -1,40 +1,29 @@
 import {
   JSXElement,
-  VirtualDOM,
-  VirtualNode,
-  VirtualDOMType,
-} from "../vtu/type";
+  ReactElementType,
+  ReactElement,
+  ReactNode,
+} from "../types/virtual-dom";
 
-export const createElement = (
-  type: VirtualDOMType,
-  props: Record<string, unknown> | null,
-  ...children: (VirtualDOM | VirtualNode | string | number)[]
+export const createElement = <P = {}>(
+  type: ReactElementType,
+  props: P & { children?: ReactNode } = {} as P & { children?: ReactNode },
+  ...children: ReactNode[]
 ): JSXElement => {
+  // 함수형 컴포넌트 처리
   if (typeof type === "function") {
-    return type({ ...props, children });
+    return createElement(type, { ...props, children });
   }
 
-  const element: VirtualDOM = {
+  const element: ReactElement<P> = {
     type,
-    props,
-    children: children.flat(Infinity).map((v) => {
-      if (!isVirtualNode(v)) {
-        return { node: v };
-      }
-      return v;
-    }) as (VirtualNode | VirtualDOMType)[],
+    props: {
+      ...props,
+      children,
+    },
   };
 
-  return { node: element };
+  return element;
 };
 
 export const jsx = createElement;
-
-const isVirtualNode = (obj: any): obj is VirtualNode => {
-  return (
-    !Array.isArray(obj) &&
-    typeof obj === "object" &&
-    obj != null &&
-    "node" in obj
-  );
-};
